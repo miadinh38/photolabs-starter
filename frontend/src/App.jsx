285742849b6627475ef95021fcc24563b40938a7
Mia Dinh <miadinh38@gmail.com>
2024-01-18 03:18:47 -0500

    Display 3 photos in total using the PhotoListItems component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
+const photos = new Array(3).fill(null).map((_, index) => ({
+  id: `${index + 1}`,
   location: {
     city: "Montreal",
     country: "Canada",
@@ -12,14 +12,16 @@ const sampleDataForPhotoListItem = {
   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
   username: "Joe Example",
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+}));
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
   return (
     <div className="App">
-      <PhotoListItem {...sampleDataForPhotoListItem}/>
+      {photos.map((data => (
+      <PhotoListItem key={data.id} {...data}/>
+    )))}
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,13 +4,15 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ imageSource, profile, username, location }) => {
 
+  const photos = new Array(3);
+
   return (
     <div className='photo-list__item'>
       <img className='photo-list__image' src={imageSource}/>
 
       <div className='photo-list__user-details'>
         <img className='photo-list__user-profile' src={profile}/>      
-        
+
         <div className='photo-list__user-info'>
           <p>{username}</p>
           <p className='photo-list__user-location'>{location.city}, {location.country}</p>
