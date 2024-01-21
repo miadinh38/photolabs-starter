90257151a0aeca408763327b485f11d1e1b54d89
Mia Dinh <miadinh38@gmail.com>
2024-01-20 23:45:18 -0500

    Modify code

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,17 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+
+  const [favouritePhoto, setFavouritePhoto] = useState(false);
+
+  const handleFavClick = () => {
+    setFavouritePhoto((prevFavourite) => !prevFavourite);
+  };
+
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        <FavIcon displayAlert={false} selected={false}/>
+        <FavIcon displayAlert={false} selected={favouritePhoto} onClick={handleFavClick}/>
       </div>
     </div>
   );
