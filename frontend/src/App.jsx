e79d413c623796614d97d02eceb34b4ea9ec9d4e
Mia Dinh <miadinh38@gmail.com>
2024-01-23 00:02:09 -0500

    Clean up code and import TopicList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,15 @@
 import React from 'react';
 
-// import PhotoListItem from './components/PhotoListItem';
-import PhotoFavButton from 'components/PhotoFavButton';
 import PhotoList from 'components/PhotoList';
+import TopicList from 'components/TopicList';
 import './App.scss';
-
-// const photos = new Array(3).fill(null).map((_, index) => ({
-//   id: `${index + 1}`,
-//   location: {
-//     city: "Montreal",
-//     country: "Canada",
-//   },
-//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-//   username: "Joe Example",
-//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-// }));
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
   return (
     <div className="App">
-      {/* <PhotoFavButton />
-      {photos.map((data => (
-      <PhotoListItem key={data.id} {...data}/>
-    )))} */}
+      <TopicList />
       <PhotoList />
     </div>
   );
