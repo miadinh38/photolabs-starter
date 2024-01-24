6032e33e31e3f8a6d1198fee06396e6c0661d77d
Mia Dinh <miadinh38@gmail.com>
2024-01-23 19:41:19 -0500

    Create the Home Page Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import PhotoList from 'components/PhotoList';
-import TopNavigation from 'components/TopNavigationBar';
+import HomeRoute from 'routes/HomeRoute';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -9,8 +8,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <TopNavigation />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import PhotoList from 'components/PhotoList';
+import TopNavigation from 'components/TopNavigationBar';
+
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation />
+      <PhotoList />
     </div>
   );
 };
