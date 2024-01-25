46e78bcc1d3d7fae321bdc11d6d903198cc323ef
Mia Dinh <miadinh38@gmail.com>
2024-01-24 21:45:13 -0500

    Create a context file to manage the global state

---
+++ frontend/src/components/FavoriteContext.js
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useState } from 'react';
+
+//creat a context object that will be used to share state across components
+const FavoriteContext = createContext(); 
+
+export const FavoriteProvider = ({ children })=> {
+
+  const [ favoritePhotos, setFavoritePhotos ] = useState([]); //hold an array of photo ID that the user has favorited.
+
+  const toggleFavorite = (photoId) => {
+    setFavoritePhotos((prevFavPhotos) => {
+      if(prevFavPhotos.includes(photoId)) {
+        return prevFavPhotos.filter((id) => id !== photoId);
+      } else {
+        return [...prevFavPhotos, photoId];
+      }
+    });
+  };
+
+  return (
+    <FavoriteContext.Provider value={{ favoritePhotos, toggleFavorite }}>
+      {children}
+    </FavoriteContext.Provider>  
+  );
+};
+
+export const useFavorite = () => {
+  return useContext(FavoriteContext);
+}
+
