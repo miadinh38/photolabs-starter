9db23b92aee3567f245ee8a52840038f5806e26a
Mia Dinh <miadinh38@gmail.com>
2024-01-27 00:46:34 -0500

    Modify component to displays a larger version of the selected photo and shows similar photos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoList from 'components/PhotoList';
 
-const PhotoDetailsModal = ({closeModal, selectedPhoto}) => {
+const PhotoDetailsModal = ({photos, urls, user, location, closeModal, selectedPhoto}) => {
   console.log('Selected Photo Details:', selectedPhoto);
 
   const handleCloseModal = () => {
@@ -15,6 +16,23 @@ const PhotoDetailsModal = ({closeModal, selectedPhoto}) => {
       <button className="photo-details-modal__close-button" onClick={handleCloseModal}>
         <img src={closeSymbol} alt="close symbol"/>
       </button>
+
+      {/* <div className="photo-details-modal__top-bar"> */}
+
+      <div className="photo-details-modal__images">
+        <img className='photo-details-modal__image' src={selectedPhoto.urls.regular} alt="photo-detail"/>
+    
+        <div className='photo-details-modal__photographer-details'>
+        <img className='photo-details-modal__photographer-profile ' src={selectedPhoto.user.profile} alt="Profile"/>
+
+
+          <span className="photo-details-modal__photographer-info">{selectedPhoto.user.name}</span><br></br>
+          <span className='photo-details-modal__photographer-location'>{selectedPhoto.location.city}, {selectedPhoto.location.country}</span>
+        </div>      
+      </div>
+
+      <span className='photo-details-modal__header'>Similar Photos</span>
+      <PhotoList photos={photos}/>
     </div>
   )
 };
