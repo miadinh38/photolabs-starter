1e85246e34433e23deeef05629ab4c6447decbdb
Mia Dinh <miadinh38@gmail.com>
2024-01-27 19:02:33 -0500

    Fix alt text

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,17 +4,14 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoList from 'components/PhotoList';
 
-const PhotoDetailsModal = ({photos, urls, user, location, closeModal, selectedPhoto}) => {
+const PhotoDetailsModal = ({closeModal, selectedPhoto}) => {
   console.log('Selected Photo Details:', selectedPhoto);
 
   const handleCloseModal = () => {
     closeModal();
   }
 
-
-  const similarPhotoArray = Object.values(selectedPhoto.similar_photos);
-  console.log('Similar Photo Array', similarPhotoArray);
-
+  const similarPhotoArray = Object.values(selectedPhoto.similar_photos); // convert similar_photos obj to arr
 
   return (
     <div className="photo-details-modal">
@@ -22,21 +19,21 @@ const PhotoDetailsModal = ({photos, urls, user, location, closeModal, selectedPh
         <img src={closeSymbol} alt="close symbol"/>
       </button>
 
-      {/* <div className="photo-details-modal__top-bar"> */}
 
       <div className="photo-details-modal__images">
-        <img className='photo-details-modal__image' src={selectedPhoto.urls.regular} alt="photo-detail"/>
+        <img className='photo-details-modal__image' src={selectedPhoto.urls.regular} alt="Selected Photo"/>
     
         <div className='photo-details-modal__photographer-details'>
-        <img className='photo-details-modal__photographer-profile ' src={selectedPhoto.user.profile} alt="Profile"/>
+          <img className='photo-details-modal__photographer-profile ' src={selectedPhoto.user.profile} alt="Profile"/>
 
-
-          <span className="photo-details-modal__photographer-info">{selectedPhoto.user.name}</span><br></br>
+          <span className="photo-details-modal__photographer-info">{selectedPhoto.user.name}</span>
           <span className='photo-details-modal__photographer-location'>{selectedPhoto.location.city}, {selectedPhoto.location.country}</span>
-        </div>      
-      </div>
+        </div>
 
-      <span className='photo-details-modal__header'>Similar Photos</span>
+        <div className="photo-details-modal__top-bar">
+          <span className='photo-details-modal__header'>Similar Photos</span>
+        </div>
+      </div>      
       <PhotoList photos={similarPhotoArray}/>
     </div>
   )
